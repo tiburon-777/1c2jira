31fb73b8ea3e16c0abb47ace6aa728103fbc5882
Andrey Ivanov <aivanov@cinimex.ru>
2020-05-25 10:53:31 +0300

    Init

---
+++ cmd/1c2jira/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/go-martini/martini"
+	"github.com/natefinch/lumberjack"
+	"log"
+	"github.com/tiburon-777/1c2jira/cmd/1c2jira/params"
+)
+
+func main() {
+	log.Println("Запускаем сервис...")
+	pms := params.GetInstance()
+	m := martini.Classic()
+	// Запускаем логи в файл и воркер, только на ПРОДЕ!
+	if pms.Development {
+		f := &lumberjack.Logger{
+			Filename:   pms.LogFile,
+			MaxSize:    5, // megabytes
+			MaxBackups: 20,
+			MaxAge:     30,   //days
+			Compress:   true, // disabled by default
+		}
+		log.SetOutput(f)
+		m.Map(log.New(f, "[martini]", log.LstdFlags))
+	}
+	//
+	// Запускаем сам сервис
+	m.RunOnAddr("localhost:8080")
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/tiburon-777/1c2jira
+
+go 1.14
+
+require (
+	github.com/codegangsta/inject v0.0.0-20150114235600-33e0aa1cb7c0 // indirect
+	github.com/go-martini/martini v0.0.0-20170121215854-22fa46961aab
+	github.com/martini-contrib/cors v0.0.0-20141016003011-553b9208d353 // indirect
+	github.com/natefinch/lumberjack v2.0.0+incompatible
+)
